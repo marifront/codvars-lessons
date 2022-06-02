576d4e21d5ec68b1dd0a8c36d768c5798fe533f4
Maria <volkova.mail@gmail.com>
2022-06-02 12:23:11 +0600

    add Credit-Card-Mask.js

---
+++ Credit-Card-Mask.js
@@ -0,0 +1,22 @@
+// function maskify(numberCard) {
+//     return numberCard.slice(-4).padStart(numberCard.length, '#')
+// }
+
+// maskify('234567');
+
+function maskify(numberCard) {
+    let str1 = '';
+    let str2 = '';
+    for(let i = 0; i<numberCard.length; i++) {
+        if(i < numberCard.length - 4) {
+            str1 += '#'
+           
+        } else {
+            str2 +=numberCard[i]
+        }
+        
+    }
+    return str1 + str2
+}
+
+maskify('234567444');
